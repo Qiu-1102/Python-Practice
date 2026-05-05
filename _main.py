e98177e48f0354d61048727b5830c5dfbc8d69dd
Qiu-1102 <wangj@wsc.school.nz>
2026-05-05 15:04:49 +1200

    start python
       ed

---
+++ _main.py
@@ -0,0 +1,11 @@
+print("hello")
+print()
+print('hello')
+print("I'm colding")
+
+print("  ---------------- ")
+print("/                  \ ")
+print("|.   O.        O    |")
+print("|         ^         |")
+print("|___________________|")
+
