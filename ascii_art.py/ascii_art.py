dd5519c7ea19455a8d16ef797c71f400184be9a7
Qiu-1102 <wangj@wsc.school.nz>
2026-05-07 09:47:16 +1200

    a
    a

--- ascii_art.py/ascii_art.py
+++ ascii_art.py/ascii_art.py
@@ -4,3 +4,20 @@ print("/                  \ ")
 print("|.   O.        O    |")
 print("|         ^         |")
 print("|___________________|")
+
+print(5+5)
+
+print(5+5.0)
+print("5+5")
+print(7*7)
+
+print("5%6+9{9=}")
+
+print("Diana+peanut")
+print("Camil|suki,camil^^")
+
+print("camil*love"^5) 
+
+
+
+
