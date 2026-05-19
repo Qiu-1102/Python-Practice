23e44738a0b329b0e89b6da5712b6f29ccca0b65
Qiu-1102 <wangj@wsc.school.nz>
2026-05-19 15:05:29 +1200

    nn

---
+++ lesson 2/madlib.py
@@ -0,0 +1,26 @@
+# Create a short Madlib: Get input from your user (a bunch of words), 
+# then output a madlib using those words.
+
+# Ask user for a name and save it in a variable
+name = input("What u name?")
+# Ask user for an animal and save it in a variable
+animal = input("what u like animal")
+# Ask user for a colour and save it in a variable
+color = input("what u like color")
+# Ask user for a  your favroit cp
+cp = input("waht ur likr cp ")
+
+# Print your madlib using the 3 variables above.
+print("you like cp: " + cp )
+# ----------------------------
+
+# EXTENSION
+# Research about 'print formatting in python'. 
+# Use what you learn to rewrite your madlib into easier to read code.
+
+# ----------------------------
+
+# EXPERT (for those who already know some Python)
+# Create a randomised madlib game
+# GOAL: Just like above except...
+#       Write 4-6 different madlibs and randomise which one is output.
---
+++ question.py
@@ -0,0 +1,59 @@
+print("hello wlecome to my qyiz")
+
+print("Question 1:")
+print("(a: auckland")
+print("(b: wellitinton")
+print("c: queenstown")
+answer = input("Question 1: whitch city is new zealnd capital city?")
+if answer == ("Wellington"):
+    print("Well done! It is Wellington. You got it right!")
+if answer == ("b"):
+     print("Well done! It is b. You got it right!")
+# if answer != ("Wellington"):
+#      print("try agin")
+else:
+     print("try again")
+
+
+# print("hi this is my secoond optin ")
+# print("question2")
+# print("Diana")
+# print("Jiayi")
+# print("Dion")
+# answer = input("Question 2: who is look good?  ")
+# if answer ==("Diana"):
+#      print("YES Diana is the most beautiful girlll ")
+# if answer ==("Jiayi"):
+#      print("YES Diana is the most beautiful girlll")
+# if answer == ("Dion"):
+#      print("YES Dion is the most handsome boyy")
+
+# print("this is high level chanllage if u wrong answer you fail")
+
+# print("Question 3:")
+# print("yes")
+# print("not")
+# answer = input("Question 2: Do you like Diana or Dion or Jiayi?")
+# if answer ==("yes"):
+#      print("yes thank you!!")
+
+# print("very nice u reach the question4")
+# print("question4")
+# print("DianaA")
+# print("DianaB")
+# print("DianaC")
+# answer = input("Question4: whitch one is right? just have one chance")
+# if answer ==("DianaA"):
+#      print("well doneeee!")
+
+# print("this is question5 final question")
+# print("A")
+# print("B")
+# print("C")
+# answer = input("Whith one is right This is final")
+# if answer ==("A"):
+#      print("OMG you won")
+
+
+
+     
