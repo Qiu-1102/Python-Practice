56727732bcbb7342b7d8e7ad6bdeb87db9ed878c
Qiu-1102 <wangj@wsc.school.nz>
2026-06-25 10:25:49 +1200

    Update question.py

--- question.py
+++ question.py
@@ -9,43 +9,45 @@ score = 0
 
 #First question:
 while True:
- print("Question 1:")
- print("(a: auckland")
- print("(b: wellitinton")
- print("c: queenstown")
- answer = input("Question 1: whitch city is new zealnd capital city?").strip().lower()
+    print("Question 1:")
+    print("(a: auckland")
+    print("(b: wellitinton")
+    print("c: queenstown")
+    question = input("Question 1: whitch city is new zealnd capital city?").strip().lower()
+    answer = ["b" , "wellington"]
  #if they get it right using Wellington
- if answer == ("wellington"):
-    print("Well done! It is Wellington. You got it right!")
-    score = score + 1
-    break
+    if question in answer:
+        print("Well done! It is Wellington. You got it right!")
+        score = score + 1
+        break
  #if they get it right using B
- elif answer == ("b"):
-     print("Well done! It is b. You got it right!")
-     score = score + 1
-     break
+    elif question == "b":
+        print("Well done! It is b. You got it right!")
+        score = score + 1
+        break
  # They get it wrong and have to try again
- else:
-     print("pls try again")
+    else:
+        print("pls try again")
 
 
 # This is the second question of my code with a function 
 while True:
 
- question = input("what ks captail city of new zealand?").strip()
+    question = input("what ks captail city of new zealand?").strip()
+    answer
  # if they get it right  using Wellington
- if question ==("Wellington"):
-     print("well done")
-     score = score + 1
-     break
+    if question ==("Wellington"):
+         print("well done")
+        score = score + 1
+        break
  # if they get it right  using wellington
- elif question ==("wellington"):
-     print("well done")
-     score = score + 1
-     break
+    elif question ==("wellington"):
+        print("well done")
+        score = score + 1
+        break
   #They get it wrong and have to try again
- else:
-    print ("pls try again ")
+    else:
+        print ("pls try again ")
 
  # This is the third question of my code with a function     
 while True:
