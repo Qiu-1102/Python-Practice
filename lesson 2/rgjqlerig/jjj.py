08abeaac7eb8595b6bff6881c27810a4fb0be9bc
Qiu-1102 <wangj@wsc.school.nz>
2026-08-20 09:40:23 +1200

    Create jjj.py

---
+++ lesson 2/rgjqlerig/jjj.py
@@ -0,0 +1,27 @@
+def game_start():
+    print:("On Wern's 48th birthday, she received a very old package, which contained a Daisy that had long withered.  The package was very old and cold, but it gave people an inexplica"
+    "ble warmth")
+    print("A. x Wern ")
+    print("wern began to investigate who sent it finally found the adress is 1997 Devonport Aucklan nz")
+    print("B. OPEN THE OLD BOOK.wern he was clean the room he found a book in the cover")
+
+    while True:
+     choice = input ("A,B").strip().upper()
+     if choice == "A":
+        print("wern began to estigate who sent it finally found the adress is 1997 Devonport Aucklan nz")
+        Autumn_Ecoho()
+        break
+        
+    
+     elif choice == "B":
+        print("iii")
+        Autumn_Ecoho()
+        break
+
+     else:
+        print("please choice A or B try agine maybe!")
+        
+
+def Autumn_Ecoho():
+    print("/n「The Autumn Ecoho」")
+    print("Wern return to 1997.The cold winds blows in deonport")
